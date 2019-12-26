2ba08110287a55efb7aa57cb0fc63f0462f2148e
Sahil Iyer <iyersahil12@gmail.com>
2019-12-27 03:12:26 +1100

    Completed day 1.

--- aoc2019/src/days/mod.rs
+++ aoc2019/src/days/mod.rs
@@ -7,18 +7,34 @@ pub fn day_1() {
 	let file = File::open(FILEPATH).unwrap();
 	let reader = BufReader::new(file);
 
-	let mut total_fuel_required = 0;
+	let mut fuel_required_for_modules_only = 0; // Part 1
+	let mut total_fuel_required = 0; // Part 2
 	
+	// NOTE(fkp): Part 1
 	for line in reader.lines() {
 		let line = line.unwrap();
 		let module_mass: i32 = line.parse().unwrap();
 		
+		fuel_required_for_modules_only += calculate_fuel_for_mass(module_mass);
 		total_fuel_required += calculate_module_fuel(module_mass);
 	}
 
-	println!("Day 1 (Part 1): Total Fuel Required = {}", total_fuel_required);
+	println!("Day 1 (Part 1): Fuel Required (Modules Only) = {}", fuel_required_for_modules_only);
+	println!("      (Part 2): Total Fuel Required = {}", total_fuel_required);
 }
 
-fn calculate_module_fuel(mass: i32) -> i32 {
+fn calculate_fuel_for_mass(mass: i32) -> i32 {
 	(mass / 3) - 2
+}
+
+fn calculate_module_fuel(module_mass: i32) -> i32 {
+	let mut total_mass = 0;
+	let mut mass = calculate_fuel_for_mass(module_mass);
+
+	while mass > 0 {
+		total_mass += mass;
+		mass = calculate_fuel_for_mass(mass);
+	}
+
+	total_mass
 }
