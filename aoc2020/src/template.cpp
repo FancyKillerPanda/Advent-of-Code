4bb98a8bfeb726d3730863e0df0b110c7db3030a
FancyKillerPanda <iyersahil12@gmail.com>
2020-12-03 18:04:42 +1100

    Added a small day template.

---
+++ aoc2020/src/template.cpp
@@ -0,0 +1,15 @@
+//  ===== Date Created: 03 December, 2020 ===== 
+
+#include <string>
+#include <vector>
+#include "utility.hpp"
+
+std::string day_CHANGE_part_one()
+{
+	std::vector<std::string> lines = read_file_lines("res/day_CHANGE.txt");
+}
+
+std::string day_CHANGE_part_two()
+{
+	std::vector<std::string> lines = read_file_lines("res/day_CHANGE.txt");
+}
